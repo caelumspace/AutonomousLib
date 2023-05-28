e6df9528740ec6e341db9699cc69c3b7f338e4b1
caelumspace <73625575+caelumspace@users.noreply.github.com>
2023-05-28 18:16:01 +0000

    Create shortest_path.py

---
+++ shortest_path.py
@@ -0,0 +1,24 @@
+import heapq
+
+def calculate_shortest_path(graph, start, end):
+    queue = []
+    heapq.heappush(queue, (0, start))
+    distances = {node: float('infinity') for node in graph}
+    distances[start] = 0
+    path = {start: []}
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+                path[neighbor] = path[current_node] + [neighbor]
+
+    return path[end]
